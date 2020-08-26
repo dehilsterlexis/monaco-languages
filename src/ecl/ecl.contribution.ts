850d408f365068d8ae2c6b4701c21232d95fb249
dehilsterlexis <david.dehilster@lexisnexisrisk.com>
2020-08-26 08:51:01 -0400

    ECL-LANG Adding ECL lexer to Monarch
    Signed-off-by: dehilsterlexis <david.dehilster@lexisnexisrisk.com>

---
+++ src/ecl/ecl.contribution.ts
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import { registerLanguage } from '../_.contribution';
+
+registerLanguage({
+	id: 'ecl',
+	extensions: ['.ecl'],
+	aliases: ['ECL', 'ecl'],
+	loader: () => import('./ecl')
+});
+registerLanguage({
+	id: 'ecl',
+	extensions: ['.ecl'],
+	aliases: ['ECL', 'Ecl', 'ecl'],
+	loader: () => import('./ecl')
+});
